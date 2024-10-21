1e68f4ed3867bf3c6b76a88085eb60865ff073e3
Isah Abdulsalam <Isahabdulsalam416@gmail.com>
2024-10-21 12:54:45 +0000

    Task1

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,6 +1,6 @@
 -- SQL script that creates a table
-
-CREATE TABLE IF NOT EXISTS users (
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
 	id INT NOT NULL AUTO_INCREMENT,
 	email VARCHAR(255) NOT NULL UNIQUE,
 	name VARCHAR(255)
