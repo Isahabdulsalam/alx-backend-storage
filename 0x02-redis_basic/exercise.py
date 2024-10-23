913619bfc23648b13873cc96af6d3bea61dac10b
Isah Abdulsalam <isahabdulsalam416@gmail.com>
2024-10-23 22:55:28 +0000

    readme file

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -4,11 +4,16 @@ import redis
 import uuid
 
 class Cache:
+    '''Represents an object for storing data in a Redis data storage.
+    '''
     def __init__(self) -> None:
         self._redis = redis.Redis()
         self._redis.flushdb(True)
         
     def store(self, data):
+        """
+        store the input data in Redis using the random key
+        """
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
